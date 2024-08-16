de04bfb593b82b8e44c56906c20591cc537a32ea
Sagar Tandan <076bct072@ioepc.edu.np>
2024-08-16 22:38:10 +0545

    frontend and backend skillss added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -80,6 +80,117 @@ const prograamLang = [
   },
 ];
 
+const frontend = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg",
+    title: "vuejs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    title: "react",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
+    title: "Svelte",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original-wordmark.svg",
+    title: "angularjs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/backbonejs/backbonejs-original-wordmark.svg",
+    title: "backbonejs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    title: "bootstrap",
+  },
+  {
+    img: "https://bestofjs.org/logos/vuetify.svg",
+    title: "vuetify",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    title: "css3",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    title: "html5",
+  },
+  {
+    img: "https://cdn.worldvectorlogo.com/logos/pug.svg",
+    title: "pug",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/gulp/gulp-plain.svg",
+    title: "gulp",
+  },
+
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+    title: "sass",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+    title: "redux",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg",
+    title: "webpack",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg",
+    title: "babeljs",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    title: "tailwindcss",
+  },
+  {
+    img: "https://raw.githubusercontent.com/prplx/svg-logos/5585531d45d294869c4eaab4d7cf2e9c167710a9/svg/materialize.svg",
+    title: "materialize",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gilbarbara/logos/804dc257b59e144eaca5bc6ffd16949752c6f789/logos/bulma.svg",
+    title: "bulma",
+  },
+];
+
+const backend = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    title: "nodejs",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+    title: "spring",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    title: "express",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+    title: "graphql",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg",
+    title: "nginx",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/apache_hadoop/apache_hadoop-icon.svg",
+    title: "hadoop",
+  },
+  {
+    img: "https://openresty.org/images/logo.png",
+    title: "openresty",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/apache_kafka/apache_kafka-icon.svg",
+    title: "kafka",
+  },
+];
+
 const Skills = () => {
   const [search, setSearch] = useState("");
   const [showText, setShowText] = useState("");
@@ -165,6 +276,91 @@ const Skills = () => {
 
         <div className="w-full grid grid-cols-4 mt-10 gap-12">
           {prograamLang.map((skill, index) => (
+            <div
+              key={index}
+              className="w-full flex gap-5 items-center oneelement"
+            >
+              <div
+                onClick={() => handleClick(skill.title)}
+                className={`checkbox border-[2px] rounded-sm w-6 h-6 border-black bg-white flex p-1`}
+              >
+                <div
+                  className={`w-full ${
+                    selectedSkills.includes(skill.title)
+                      ? "bg-purple-600 rounded-sm"
+                      : "bg-white"
+                  }`}
+                ></div>
+              </div>
+              <img
+                onClick={() => handleClick(skill.title)}
+                onMouseEnter={() => setShowText(skill.title)}
+                onMouseLeave={() => setShowText("")}
+                className="w-10 h-10"
+                src={skill.img}
+                alt={skill.title}
+              />
+              <h1
+                className={`px-4 py-1 bg-black text-white transform transition-opacity duration-500 ${
+                  showText === skill.title ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                {skill.title}
+              </h1>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="programming w-full flex flex-col mt-10">
+        <h1 className="w-full text-xl">Frontend Development</h1>
+        <hr className="w-full mt-1 border-[1px] border-[#d3d3d3]" />
+
+        <div className="w-full grid grid-cols-4 mt-10 gap-12">
+          {frontend.map((skill, index) => (
+            <div
+              key={index}
+              className="w-full flex gap-5 items-center oneelement"
+            >
+              <div
+                onClick={() => handleClick(skill.title)}
+                className={`checkbox border-[2px] rounded-sm w-6 h-6 border-black bg-white flex p-1`}
+              >
+                <div
+                  className={`w-full ${
+                    selectedSkills.includes(skill.title)
+                      ? "bg-purple-600 rounded-sm"
+                      : "bg-white"
+                  }`}
+                ></div>
+              </div>
+              <img
+                onClick={() => handleClick(skill.title)}
+                onMouseEnter={() => setShowText(skill.title)}
+                onMouseLeave={() => setShowText("")}
+                className="w-10 h-10"
+                src={skill.img}
+                alt={skill.title}
+              />
+              <h1
+                className={`px-4 py-1 bg-black text-white transform transition-opacity duration-500 ${
+                  showText === skill.title ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                {skill.title}
+              </h1>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Backend Development */}
+      <div className="programming w-full flex flex-col mt-10">
+        <h1 className="w-full text-xl">Backend Development</h1>
+        <hr className="w-full mt-1 border-[1px] border-[#d3d3d3]" />
+
+        <div className="w-full grid grid-cols-4 mt-10 gap-12">
+          {backend.map((skill, index) => (
             <div
               key={index}
               className="w-full flex gap-5 items-center oneelement"
