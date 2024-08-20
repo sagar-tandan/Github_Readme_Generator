8abd7f6c7b1bdcd8b5f516180ad1664eaf1e4159
Sagar Tandan <076bct072@ioepc.edu.np>
2024-08-20 13:02:32 +0545

    Header completed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,26 @@ import logo from "../assets/logoo.webp";
 
 const Header = () => {
   return (
-    <div className="w-full flex py-4 fixed bg-white">
+    <div className="w-full flex py-4 fixed bg-white px-4 sm:px-0 z-50">
       <div className="w-full max-w-screen-2xl flex justify-between mx-auto">
         <div className="w-full flex gap-2 items-center">
-          <img className="w-12 h-12" src={logo} alt="" />
-          <h1 className="text-3xl font-bold text-[#691a99]">GRMG</h1>
+          <img className="w-6 h-6 sm:w-12 sm:h-12" src={logo} alt="" />
+          <h1 className="sm:text-3xl font-bold text-[#691a99] text-md ">GRMG</h1>
         </div>
+        <a
+          href="https://buymeacoffee.com/sagartandan_"
+          target="_blank"
+          className="w-full flex gap-2 items-center justify-end "
+        >
+          <button className="flex bg-[#ffdd00] px-2 py-1 items-center justify-center rounded-md active:scale-90 duration-100 transition-all">
+            <img
+              className="w-6 h-6 sm:w-9 sm:h-9 "
+              src="https://koronapos.com/wp-content/uploads/2020/04/684569_Landing-Page-Icons-Coffee-Shop-_041320.png"
+              alt=""
+            />
+            <h1 className="sm:text-xl font-semibold text-md ">Donate us</h1>
+          </button>
+        </a>
       </div>
     </div>
   );
